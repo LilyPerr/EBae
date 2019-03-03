c48aca160a1fbafa59a037b6b3547e5f58ba5eed
whoisis <readatontim@gmail.com>
2019-03-02 22:24:40 -0800

    Adding css, html, js files

---
+++ eBae.js
@@ -0,0 +1,10 @@
+var stuff = document.getElementById('racistMF')
+var table = document.getElementById('table1')
+var line1 = table.insertRow(1)
+var item = line1.insertCell()
+var cost = line1.insertCell()
+var button = line1.insertCell()
+
+item.innerHTML = 'Popcorn Chicken'
+cost.innerHTML = '$1.5'
+button.innerHTML = 'Button'
