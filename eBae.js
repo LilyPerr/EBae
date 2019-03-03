d2a40ea3ae31e0b2c12488c7440a97b4811d90e2
whoisis <readatontim@gmail.com>
2019-03-03 00:31:50 -0800

    created clubside still need firebase dict implimencation

--- eBae.js
+++ eBae.js
@@ -4,7 +4,18 @@ var line1 = table.insertRow(1)
 var item = line1.insertCell()
 var cost = line1.insertCell()
 var button = line1.insertCell()
+var itemsInCart = document.getElementById('numItems')
+var items = 0
+itemsInCart.innerHTML = items
+var amount = 1.5
+
 
 item.innerHTML = 'Popcorn Chicken'
-cost.innerHTML = '$1.5'
-button.innerHTML = '<button class = "addToCart" type = "button"> Add To Cart </button>'
+cost.innerHTML = '$' + String(amount)
+button.innerHTML = '<button class = "addToCart" type = "button" onclick = "add();"> Add To Cart </button>'
+
+function add(){
+  //push to firebase and refresh page
+  items += 1
+  itemsInCart.innerHTML = items
+}
