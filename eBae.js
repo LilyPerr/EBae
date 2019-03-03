3ff99b9e6ea6322667441d6b656b41b1d4aa0adf
whoisis <readatontim@gmail.com>
2019-03-03 04:17:00 -0800

    eBae edits

--- eBae.js
+++ eBae.js
@@ -2,20 +2,28 @@ var stuff = document.getElementById('racistMF')
 var table = document.getElementById('table1')
 var line1 = table.insertRow(1)
 var item = line1.insertCell()
-var cost = line1.insertCell()
+
 var button = line1.insertCell()
 var itemsInCart = document.getElementById('numItems')
+var row = document.getElementById('row')
 var items = 0
-itemsInCart.innerHTML = items
-var amount = 1.5
 
+if (0 == 1){
+  var cost = line1.insertCell(1)
+  var manage = row.insertCell(1)
+  manage.innerHTML = 'Manage Event' // link to event management page
+  //item.innerHTML = event Name
+  cost.innerHTML = '<button class = "addToCart" type = "button" onclick = "add();"> Edit Event </button>'
+  button.innerHTML = '<button class = "addToCart" type = "button" onclick = "add();"> Engage in Event </button>'
+}
+else{
 
-item.innerHTML = 'Popcorn Chicken'
-cost.innerHTML = '$' + String(amount)
-button.innerHTML = '<button class = "addToCart" type = "button" onclick = "add();"> Add To Cart </button>'
+  //item.innerHTML = event Name
+  button.innerHTML = '<button class = "addToCart" type = "button" onclick = "add();"> Engage in Event </button>'
+}
 
 function add(){
-  //push to firebase and refresh page
+  //push to firebase and refresh
   items += 1
   itemsInCart.innerHTML = items
 }
